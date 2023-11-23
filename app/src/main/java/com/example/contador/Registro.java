ac4b4ea6500f336e38d63462b1b9fa86278881e1
Claudia Casero Álvarez <dms80558@educastur.es>
2023-11-23 14:04:03 +0100

    creacion bd

--- app/src/main/java/com/example/contador/Registro.java
+++ app/src/main/java/com/example/contador/Registro.java
@@ -1,15 +1,31 @@
 package com.example.contador;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Registro extends AppCompatActivity {
 
+
+    EditText user, password,conf_password;
+    Button confimar;
+    DBHelper DB;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inciarsesion);
 
+        user = findViewById(R.id.user);
+        password = findViewById(R.id.password);
+        conf_password = findViewById(R.id.confim_password);
+        confimar = findViewById(R.id.registarse);
+        DB = new DBHelper(this);
     }
 }
